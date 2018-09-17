b3edd2559d6ba3d853190e485846bcf32fcb5446
Alvaro Leal <pursuance@gmail.com>
2018-09-17 16:22:27 +0200

    Update footer, header, nav section and root

--- app/javascript/app/router/router.js
+++ app/javascript/app/router/router.js
@@ -2,14 +2,9 @@ import { connectRoutes, NOT_FOUND, redirect } from 'redux-first-router';
 import createHistory from 'history/createBrowserHistory';
 import queryString from 'query-string';
 
-import NationalSections from './sections/national-circumstances';
-import GHGSections from './sections/ghg-emissions';
-
 const history = createHistory();
 
 export const HOME = 'location/HOME';
-export const NATIONAL_CIRCUMSTANCES = 'location/NATIONAL_CIRCUMSTANCES';
-export const GHG_EMISSIONS = 'location/GHG_EMISSIONS';
 
 export const routes = {
   [HOME]: {
@@ -17,22 +12,6 @@ export const routes = {
     label: 'Overview',
     path: '/',
     component: 'pages/home/home'
-  },
-  [NATIONAL_CIRCUMSTANCES]: {
-    nav: true,
-    label: 'National Circumstances',
-    link: '/national-circumstances',
-    path: '/national-circumstances/:section?',
-    component: 'layouts/sections/sections',
-    sections: NationalSections
-  },
-  [GHG_EMISSIONS]: {
-    nav: true,
-    label: 'GHG Emissions',
-    link: '/ghg-emissions',
-    path: '/ghg-emissions/:section?',
-    component: 'layouts/sections/sections',
-    sections: GHGSections
   },
   [NOT_FOUND]: {
     path: '/404',

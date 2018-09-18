b85f10fa8128f4ad3438d9b27cc601459409f276
j8seangel <jseangel@gmail.com>
2018-09-18 19:02:40 +0200

    Use hot module reloading

--- app/javascript/packs/main.jsx
+++ app/javascript/packs/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'app';
+import { AppContainer } from 'react-hot-loader';
 
-document.addEventListener('DOMContentLoaded', () => {
+const render = Component => {
   ReactDOM.render(
-    <App data={Object.assign({}, window.__data)} />,
-    document.querySelector('#root')
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
   );
-});
+};
+
+document.addEventListener('DOMContentLoaded', () => render(App));
+
+if (module.hot) {
+  /* eslint-disable global-require */
+  module.hot.accept('../app', () => {
+    render(require('../app').default);
+  });
+}
--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,6 +3,7 @@
 // eslint-disable-next-line
 const dotenv = require('dotenv').config();
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const sharedConfig = require('./shared.js');
 const { settings, output } = require('./configuration.js');
@@ -12,7 +13,7 @@ module.exports = merge(sharedConfig, {
   mode: 'development',
   stats: { errorDetails: true },
   output: { pathinfo: true },
-  plugins: [],
+  plugins: [ new webpack.HotModuleReplacementPlugin() ],
   devServer: {
     clientLogLevel: 'none',
     https: settings.dev_server.https,
@@ -23,6 +24,8 @@ module.exports = merge(sharedConfig, {
     compress: true,
     headers: { 'Access-Control-Allow-Origin': '*' },
     historyApiFallback: true,
-    watchOptions: { ignored: /node_modules/ }
+    watchOptions: { ignored: /node_modules/ },
+    hot: true,
+    hotOnly: true
   }
 });
